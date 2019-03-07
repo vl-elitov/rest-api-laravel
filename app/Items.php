763699196b483b57ba33627327914b8458438ed9
Vladimir Anufriev <anufrievv.vvladimir@gmail.com>
2019-03-07 14:19:00 +0300

    Items rest api based

---
+++ app/Items.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Items
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Items newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Items newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Items query()
+ * @mixin \Eloquent
+ */
+class Items extends Model
+{
+    //
+}
